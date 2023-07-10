e74d6193cffed7433c686d69dfea71d3e7a65f03
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-07-09 22:04:09 -0300

    tirando comentário

--- exercíciosJSManzano/ExerciciosEnquanto/exercicioh/js/script.js
+++ exercíciosJSManzano/ExerciciosEnquanto/exercicioh/js/script.js
@@ -8,19 +8,3 @@ function exercicioH(){
  }
 }
 
-// Var
-// // Seção de Declarações das variáveis 
-// Celsius,Fah,conversao:real
-
-// Inicio
-// // Seção de Comandos, procedimento, funções, operadores, etc... 
-// Celsius<-10
-// enquanto Celsius<=100 faca
-// conversao<-((9*Celsius)+160)/5
-// Fah<-conversao
-// escreval("Temperatura em Celsius: ",Celsius,"   Temperatura em Fahrenheit: ",Fah)
-// Celsius<-Celsius+10
-
-// fimenquanto
-
-// Fimalgoritmo
